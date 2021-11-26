f6bad31667224f377bedf69cf3926496c44e47e4
BarbonFisso <barbieri.mat@hotmail.it>
2021-11-26 14:30:45 +0100

    added discs section

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,40 @@
 <template>
   <div id="app">
+    <header>
+
+    </header>
+    <main>
+      <Discs/>
+    </main>
   </div>
 </template>
 
 <script>
+import Discs from "@/components/Discs.vue";
 
 export default {
   name: 'App',
   components: {
+    Discs
   }
 }
 </script>
 
 <style lang="scss">
+* {
+  box-sizing: border-box;
+  padding: 0;
+  margin: 0;
+}
 
+header {
+  background-color: #2e3a46;
+  height: 80px;
+  width: 100vw;
+}
+main {
+  background-color: #1e2d3b;
+  height: calc(100vh - 80px);
+  width: 100vw;
+}
 </style>
---
+++ src/components/Discs.vue
@@ -0,0 +1,18 @@
+<template>
+  <section id="discs-container">
+
+  </section>
+</template>
+
+<script>
+
+export default {
+  name: 'Discs',
+  components: {
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
